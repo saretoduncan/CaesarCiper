7bb683ac45721682eba7cfb03b8f8b76f4caf593
saretoduncan <saretoduncan@gmail.com>
2021-07-30 18:22:38 +0300

    add test to alphabetic class

--- src/test/java/backend/alphabeticTest.java
+++ src/test/java/backend/alphabeticTest.java
@@ -1,8 +1,14 @@
 package backend;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class alphabeticTest {
-
-
+    @Test
+    void alphabeticTest_getterMethod() {
+        Alphabetic testAlphabet = new Alphabetic();
+        char[] results = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        assertArrayEquals(results, testAlphabet.getLetterOfAlphabet());
+    }
 }
