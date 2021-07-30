9b727f85cfc61dc62c8199a4df5765e00432095f
saretoduncan <saretoduncan@gmail.com>
2021-07-30 18:21:20 +0300

    add private alphabetic array with alphabetic letters and a getter

---
+++ src/main/java/backend/Alphabetic.java
@@ -0,0 +1,9 @@
+package backend;
+
+public class Alphabetic {
+ private char[] letterOfAlphabet={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public char[] getLetterOfAlphabet() {
+        return letterOfAlphabet;
+    }
+}
