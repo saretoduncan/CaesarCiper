e40c88e905c1acb5b3c6f0e989f3e9b14568c410
saretoduncan <saretoduncan@gmail.com>
2021-07-31 20:51:35 +0300

    add scanner to take in user input and gives encrypted and decrypted messages

--- src/main/java/App/main.java
+++ src/main/java/App/main.java
@@ -1,8 +1,27 @@
 package App;
 
+import backend.CeaserLogic;
+
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter message to be encrypted:");
+        String word=scan.nextLine();
+        System.out.println("Please enter shift key");
+        int shiftKey= scan.nextInt();
+        CeaserLogic encryption= new CeaserLogic(word,shiftKey);//backend class
+        String encrypted_message= encryption.getEncoded();//encryption
+        String decrypted_message= encryption.getDecord();//decryption
+        printMessage(encrypted_message,decrypted_message);
 
+
+    }
+
+    private static void printMessage(String encrypted_message, String decrypted_message) {
+        System.out.println("Encrypted message: " + encrypted_message);
+        System.out.println("Decrypted message: "+ decrypted_message);
     }
 
 }
