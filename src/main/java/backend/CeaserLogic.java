8befb3885e54fe34f81e824398ffb3461f6df0a2
saretoduncan <saretoduncan@gmail.com>
2021-07-31 16:58:38 +0300

    generate encoding getter and test method for message

--- src/main/java/backend/CeaserLogic.java
+++ src/main/java/backend/CeaserLogic.java
@@ -1,20 +1,51 @@
 package backend;
 
-import java.util.Locale;
+
 
 public class CeaserLogic {
     private char[] toArr;
     private String word;
+    private int key;
+    private char[] letterOfAlphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    private String encoded;
 
 
 
-    public CeaserLogic(String word) {
+
+    public CeaserLogic(String word,int key) {
       this.word=  word.toUpperCase();
 
-      this.toArr=this.word.toCharArray();
+      this.key=key;
     }
 
-    public char[] getToArr() {
-        return toArr;
+    public String getWord() {
+        return word;
+    }
+
+    public String getEncoded() {
+        int encryptionIndex;
+         String str="";
+         String message=getWord();
+        for (int j = 0; j < message.length(); j++) {
+            int finalIndex;
+            char strChar = message.charAt(j);
+
+            int index = new String(letterOfAlphabet).indexOf(strChar);
+            if (index != -1) {
+                encryptionIndex = index + this.key;
+
+                if (encryptionIndex <= 25) {
+                    finalIndex = encryptionIndex;
+                } else {
+                    finalIndex = encryptionIndex % 26;
+                }
+                str += this.letterOfAlphabet[finalIndex];
+            }
+        }
+
+       return encoded= str;
+
+
+
     }
 }
--- src/test/java/backend/CeaserLogicTest.java
+++ src/test/java/backend/CeaserLogicTest.java
@@ -7,14 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class CeaserLogicTest {
     @Test
     void testForStingInput() {
-        CeaserLogic testceaser= new CeaserLogic("hi");
+        CeaserLogic testceaser= new CeaserLogic("hi",1);
         assertEquals(true, testceaser instanceof CeaserLogic);
     }
     @Test
     void testForString_toCharArray() {
-        CeaserLogic testceaser= new CeaserLogic("hello everyone");
+        CeaserLogic testceaser= new CeaserLogic("hello everyone",3);
         String str="hello everyone".toUpperCase();
-        char[] results=str.toCharArray();
-        assertArrayEquals(results, testceaser.getToArr());
+
+        assertEquals(str, testceaser.getWord());
+    }
+
+    @Test
+    void encryptionTest() {
+        CeaserLogic testceaser= new CeaserLogic("hell",3);
+
+        assertEquals("KHOO", testceaser.getEncoded());
     }
 }
